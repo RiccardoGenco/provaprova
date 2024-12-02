ab0bc489d834a5692ed7ded661e1b7894483a330
Utente <Utente@192.168.1.53>
2024-12-02 15:35:44 +0100

    modifica in "questo è il branch modificato"

--- src/prova/rami.java
+++ src/prova/rami.java
@@ -3,7 +3,7 @@ package prova;
 public class rami {
 
 	public static void main(String[] args) {
-		System.out.println("questo è il branch.");
+		System.out.println("questo è il branch modificato.");
 
 	}
 
